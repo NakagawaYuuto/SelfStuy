6b866064dcf91ee98c74af8d441a6262274a0ecf
yuutonakagawa <github.nakagawa@gmail.com>
2022-06-23 00:28:09 +0900

    メンバ変数

---
+++ MemberVariable.java
@@ -0,0 +1,7 @@
+public class MemberVariable {
+    String MemberVariable = "メンバ変数";//これがメンバ変数
+
+    public static void main(String[] args) {
+//        String型の　MemberVariable メンバ変数には文字列を格納できる
+    }
+}
